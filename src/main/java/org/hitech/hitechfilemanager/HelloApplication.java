748188630849edde88065433a077ea17b9e795c1
= <=>
2023-02-13 16:10:45 +0100

    I've added a new fxml file that tests as the dark theme for the project and also added the imageview object that serve as a network representation to the program

--- src/main/java/org/hitech/hitechfilemanager/HelloApplication.java
+++ src/main/java/org/hitech/hitechfilemanager/HelloApplication.java
@@ -3,6 +3,7 @@ package org.hitech.hitechfilemanager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("filepage-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+//        Label titleLabel = new Label("Hi-Tech File Manager");
+        stage.setTitle("Hi-Tech File Manager");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
